e6bedc6053cd827344086d46ec224bc39fdbd5d8
glgdanshall <dan@greenlightgroup.com>
2021-02-26 13:15:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
 var username = document.querySelector(".ess-avatar-widget-user").innerHTML.trim();
 alert('my username is: '+ username); 
-
-
-document.getElementClassName("ess-category-item-background").style.backgroundColor = "yellow";
-
